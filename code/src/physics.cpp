da7c4af0cd728a43cdf384ecee829a1b29a3822f
Miguel <miguel.alvarez@enti.cat>
2019-02-06 09:27:26 +0100

    1st push

---
+++ code/src/physics.cpp
@@ -0,0 +1,112 @@
+#include <imgui\imgui.h>
+#include <imgui\imgui_impl_sdl_gl3.h>
+#include <glm\gtc\matrix_transform.hpp>
+
+namespace Box {
+	void drawCube();
+}
+namespace Axis {
+	void drawAxis();
+}
+
+namespace Sphere {
+	extern void updateSphere(glm::vec3 pos, float radius = 1.f);
+	extern void drawSphere();
+}
+namespace Capsule {
+	extern void updateCapsule(glm::vec3 posA, glm::vec3 posB, float radius = 1.f);
+	extern void drawCapsule();
+}
+namespace Particles {
+	extern const int maxParticles;
+	extern void updateParticles(int startIdx, int count, float* array_data);
+	extern void drawParticles(int startIdx, int count);
+}
+namespace Mesh {
+	extern const int numCols;
+	extern const int numRows;
+	extern void updateMesh(float* array_data);
+	extern void drawMesh();
+}
+namespace Fiber {
+extern const int numVerts;
+	extern void updateFiber(float* array_data);
+	extern void drawFiber();
+}
+namespace Cube {
+	extern void updateCube(const glm::mat4& transform);
+	extern void drawCube();
+}
+
+
+
+// Boolean variables allow to show/hide the primitives
+bool renderSphere = false;
+bool renderCapsule = false;
+bool renderParticles = false;
+bool renderMesh = false;
+bool renderFiber = false;
+bool renderCube = false;
+
+//You may have to change this code
+void renderPrims() {
+	Box::drawCube();
+	Axis::drawAxis();
+
+
+	if (renderSphere)
+		Sphere::drawSphere();
+	if (renderCapsule)
+		Capsule::drawCapsule();
+
+	if (renderParticles) {
+		int startDrawingFromParticle = 0;
+		int numParticlesToDraw = Particles::maxParticles;
+		Particles::drawParticles(startDrawingFromParticle, numParticlesToDraw);
+	}
+
+	if (renderMesh)
+		Mesh::drawMesh();
+	if (renderFiber)
+		Fiber::drawFiber();
+
+	if (renderCube)
+		Cube::drawCube();
+}
+
+
+void GUI() {
+	bool show = true;
+	ImGui::Begin("Physics Parameters", &show, 0);
+
+	// Do your GUI code here....
+	{	
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);//FrameRate
+		
+	}
+	// .........................
+	
+	ImGui::End();
+
+	// Example code -- ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+	bool show_test_window = false;
+	if(show_test_window) {
+		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+		ImGui::ShowTestWindow(&show_test_window);
+	}
+}
+
+void PhysicsInit() {
+	// Do your initialization code here...
+	// ...................................
+}
+
+void PhysicsUpdate(float dt) {
+	// Do your update code here...
+	// ...........................
+}
+
+void PhysicsCleanup() {
+	// Do your cleanup code here...
+	// ............................
+}
